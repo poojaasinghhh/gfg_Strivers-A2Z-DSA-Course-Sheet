d946ce181ca5fab619112794c3c549a8a8e1b6f4
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2023-06-22 00:19:40 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern 7/pattern-7.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                printf(" ");
+            }
+            for(int k=1;k<2*i;k++){
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
