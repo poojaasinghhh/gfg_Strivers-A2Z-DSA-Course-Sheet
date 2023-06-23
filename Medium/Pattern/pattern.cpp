194cbf52b895effd28cf22c9aa53b35ba95ab6d3
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2023-06-23 22:14:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Pattern/pattern.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printDiamond(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                printf(" ");
+            }
+            for(int j=0;j<=i;j++){
+                printf("* ");
+            }
+            printf("\n");
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                printf(" ");
+            }
+            for(int j=n-i;j>0;j--){
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printDiamond(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
