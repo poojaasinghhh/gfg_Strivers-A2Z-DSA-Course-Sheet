648a17697777d8bec4d848f210b126e294b072be
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2023-06-24 23:03:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 16/pattern-16.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int start=65;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<char(start);
+            }
+            start=start+1;
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
