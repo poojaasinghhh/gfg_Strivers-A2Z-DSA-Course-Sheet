c23df1a5517db2b2bf2f2f334bb7d787b96b516d
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2023-06-26 00:30:26 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindrome/palindrome.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    int ld,rev=0,num=n;
+		    while(num!=0){
+		        ld=num%10;
+		        rev=rev*10+ld;
+		        num=num/10;
+		    }
+		    if(n==rev){
+		        return "Yes";
+		    }
+		    return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
