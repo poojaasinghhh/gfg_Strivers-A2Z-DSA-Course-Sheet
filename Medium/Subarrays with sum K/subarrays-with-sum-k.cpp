3a4d038af62f5c03cebb9cd03bb6706ca1c50583
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2024-01-11 02:50:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSubArraySum(int Arr[], int N, int k)
+    {
+        // code here
+        map<int,int>ans;
+        ans[0]=1;
+        int preSum=0,count=0;
+        for(int i=0;i<N;i++){
+            preSum+=Arr[i];
+            int rem=preSum-k;
+            count+=ans[rem];
+            ans[preSum]+=1;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.findSubArraySum(Arr, N, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
