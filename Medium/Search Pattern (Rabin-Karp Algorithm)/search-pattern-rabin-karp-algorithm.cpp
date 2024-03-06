9d693d617a9118b3b4183247eedc4ffdb25627b0
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2024-03-07 02:43:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        
+     vector<int> computeLPS(string pattern) {
+    int m = pattern.length();
+    vector<int> lps(m, 0);
+    int len = 0;
+    int i = 1;
+    
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            }else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    
+    return lps;
+}
+
+vector <int> search(string pattern, string text) {
+    vector<int> indices;
+    int m = pattern.length();
+    int n = text.length();
+    vector<int> lps = computeLPS(pattern);
+    int i = 0; 
+    int j = 0; 
+
+    while (i < n) {
+        if (pattern[j] == text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            indices.push_back(i - j + 1);
+            j = lps[j - 1];
+        } else if (i < n && pattern[j] != text[i]) {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+
+    return indices;
+}
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
