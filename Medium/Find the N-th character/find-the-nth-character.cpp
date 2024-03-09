04233bfb25b425b3c18eefd9494f31344c65f13d
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2024-03-09 22:20:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the N-th character/find-the-nth-character.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    char nthCharacter(string s, int r, int n) {
+        //code here
+           int cnt = 0;
+        while(r!=0){
+            cnt += n%2;
+            n /= 2;
+            r--;
+        }
+        if(cnt %2==0) return s[n];
+        else return s[n] == '0' ? '1' : '0';
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int R, N;
+        string S;
+        cin >> S >> R >> N;
+        Solution ob;
+        cout << ob.nthCharacter(S, R, N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
