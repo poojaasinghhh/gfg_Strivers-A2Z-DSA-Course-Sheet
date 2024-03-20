0c9648d4a0a2c8d667c5dbdbe1157019c9e19432
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2024-03-21 01:59:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -0,0 +1,173 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.util.HashMap;
+import java.io.*;
+
+    class Node
+    {
+        int data;
+        Node left,right;
+        Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+    }
+		
+public class GFG2
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void inorder(Node root)
+	{
+	    if(root==null)
+	    return;
+	    inorder(root.left);
+	    System.out.print(root.data);
+	    inorder(root.right);
+	}
+     /* Drier program to test above functions */
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    GFG g = new GFG();
+			
+			    ArrayList<Integer> res = g.zigZagTraversal(root) ;
+			    for (int i = 0; i < res.size (); i++)
+			        System.out.print (res.get (i) + " ");
+			    System. out. println();  
+    			
+                t--;
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class GFG
+{
+    //Function to store the zig zag order traversal of tree in a list.
+	ArrayList<Integer> zigZagTraversal(Node root)
+	{
+	    //Add your code here.
+	    ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        // Create a deque to store nodes at each level
+        Deque<Node> deque = new LinkedList<>();
+        deque.offer(root);
+        boolean leftToRight = true;
+
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            ArrayList<Integer> levelNodes = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                Node node;
+                if (leftToRight) {
+                    node = deque.pollFirst(); // Remove node from the front
+                    if (node.left != null) {
+                        deque.offerLast(node.left); // Add left child to the back
+                    }
+                    if (node.right != null) {
+                        deque.offerLast(node.right); // Add right child to the back
+                    }
+                } else {
+                    node = deque.pollLast(); // Remove node from the back
+                    if (node.right != null) {
+                        deque.offerFirst(node.right); // Add right child to the front
+                    }
+                    if (node.left != null) {
+                        deque.offerFirst(node.left); // Add left child to the front
+                    }
+                }
+                levelNodes.add(node.data);
+            }
+
+            result.addAll(levelNodes);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+
+	}
+}
