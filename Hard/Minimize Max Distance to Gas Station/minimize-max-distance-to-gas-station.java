bae69b62a840e457345173b02eaca07fc6910525
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2024-05-22 23:38:53 +0530

    Updated Solution - GfG to GitHub

--- Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.java
+++ Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.java
@@ -1,38 +1,37 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.*;
+import java.io.*;
 import java.lang.*;
 import java.math.*;
-import java.io.*;
+import java.util.*;
 
 class GFG {
-  public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    while (T-- > 0) {
-      int n = sc.nextInt();
-      int a[] = new int[n];
-      for(int i=0;i<n;i++){
-        a[i]=sc.nextInt();
-      }
-      int k = sc.nextInt();
-      Solution obj = new Solution();
-      double ans = obj.findSmallestMaxDist(a,k);
-      System.out.printf("%.2f",ans);
-      System.out.println();
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            Solution obj = new Solution();
+            double ans = obj.findSmallestMaxDist(a, k);
+            System.out.printf("%.2f", ans);
+            System.out.println();
+        }
     }
-  }
 }
-
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
 class Solution {
-  public static int noOfGasStationsBetween(int[] arr, double dist) {
+    public static int noOfGasStationsBetween(int[] arr, double dist) {
     int cnt = 0, n = arr.length;
 
     for (int i = 0; i < n - 1; i++) {
@@ -61,4 +60,3 @@ class Solution {
     return high;
   }
 }
-     
