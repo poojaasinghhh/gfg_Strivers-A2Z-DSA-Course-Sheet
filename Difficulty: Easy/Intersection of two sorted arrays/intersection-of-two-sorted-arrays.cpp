d71db9534327d1f8b3636a644523ca56e84affce
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2024-09-23 20:52:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the intersection of two arrays.
+    vector<int> printIntersection(vector<int> &arr1, vector<int> &arr2) {
+        // Your code here
+        
+        vector<int> ans;
+    int i = 0, j = 0;
+    
+    while (i < arr1.size() && j < arr2.size()) {
+        if (arr1[i] < arr2[j]) {
+            i++;
+        } else if (arr2[j] < arr1[i]) {
+            j++;
+        } else {  // arr1[i] == arr2[j]
+            // Ensure the result is unique
+            if (ans.empty() || ans.back() != arr1[i]) {
+                ans.push_back(arr1[i]);
+            }
+            i++;
+            j++;
+        }
+    }
+    
+    if(ans.size()==0){
+        ans.push_back(-1);
+    }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> arr1, arr2;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            arr1.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            arr2.push_back(number2);
+        }
+        Solution ob;
+        // Function calling
+        vector<int> v;
+        v = ob.printIntersection(arr1, arr2);
+
+        for (int i = 0; i < v.size(); i++)
+            cout << v[i] << " ";
+
+        cout << endl;
+        // string tilde;
+        // getline(cin, tilde);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
