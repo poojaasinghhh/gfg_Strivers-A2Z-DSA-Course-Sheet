b1ba6da0f9c18d836bb33d6cdbd8d392ef3cc662
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2025-03-18 21:31:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Duplicates from unsorted array/remove-duplicates-from-unsorted-array.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        // code here
+        
+        set<int>temp;
+        
+        vector<int>ans;
+        
+        
+        for(auto i:arr){
+            if(temp.find(i)==temp.end()){
+                temp.insert(i);
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.removeDuplicate(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
